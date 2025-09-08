7bcaf1faab448c120ff8db7f73ac5f6ac6b626ee
Sam Ansmink <samansmink@hotmail.com>
2025-09-08 16:34:52 +0200

    fix: etag checking bug

--- extension/httpfs/httpfs.cpp
+++ extension/httpfs/httpfs.cpp
@@ -232,7 +232,7 @@ unique_ptr<HTTPResponse> HTTPFileSystem::GetRangeRequest(FileHandle &handle, str
 		    if (static_cast<int>(response.status) < 300) { // done redirecting
 			    out_offset = 0;
 
-			    if (!hfh.http_params.unsafe_disable_etag_checks && hfh.etag.empty() && response.HasHeader("ETag")) {
+			    if (!hfh.http_params.unsafe_disable_etag_checks && !hfh.etag.empty() && response.HasHeader("ETag")) {
                                 string responseEtag = response.GetHeaderValue("ETag");
 
                                 if (!responseEtag.empty() && responseEtag != hfh.etag) {
